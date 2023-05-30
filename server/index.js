469ca72ad8d3f361fbc63892a012883a2c031ed0
Abubakar Shaikh <64248752+abubakar-shaikh-dev@users.noreply.github.com>
2023-05-30 20:57:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,6 +20,14 @@ import dotenv from 'dotenv';
     //BodyParser
         app.use(bodyParser.json())
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.get("/",(req,res)=>{
     res.status(403).send("Forbidden!")
 })
