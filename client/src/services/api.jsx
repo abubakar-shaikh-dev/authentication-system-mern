8d6e8cabe8066752cd49b244488fb470fcc9ab60
Abubakar Shaikh <64248752+abubakar-shaikh-dev@users.noreply.github.com>
2023-05-31 07:46:31 +0530

    Update api.jsx

--- client/src/services/api.jsx
+++ client/src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "https://authentication-system-mern-backend.vercel.app/api"
+const URL = "https://authentication-system-mern.vercel.app/api"
 
 export async function registerUser(data){
     try {
