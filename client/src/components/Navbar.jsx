d871a1b2988f1b4b217cc5c3f3252fd6e9b8b0cc
Abubakar Shaikh <64248752+abubakar-shaikh-dev@users.noreply.github.com>
2023-06-08 12:39:16 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -25,7 +25,7 @@ export default function Navbar() {
             <div className="dropdown dropdown-end">
               <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                 <div className="w-10 rounded-full">
-                  <img src="src/assets/images/user.png" />
+                  <img src="assets/images/user.png" />
                 </div>
               </label>
               <ul
